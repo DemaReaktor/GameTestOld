b6aaaf0dfdc8a6de5c0b6b294c36e19cb35da114
Dema <67798322+DemaReaktor@users.noreply.github.com>
2023-02-12 01:05:01 +0200

    Configuration

--- Assets/Scripts/Architecture/ManagerConfiguration.cs
+++ Assets/Scripts/Architecture/ManagerConfiguration.cs
@@ -8,6 +8,7 @@ namespace GameArchitecture
     [Serializable]
     public class ManagerConfiguration
     {
+        [Tooltip("class should be direved by IManager")]
         public MonoScript MonoScript;
         [SerializeReference] public object Configuration;
     }
@@ -16,8 +17,8 @@ namespace GameArchitecture
     public class ManagerConfigurationEditor : PropertyDrawer
     {
         private MonoScript MonoScript = null;
-        private MonoScript NewMonoScript = null;
-        private bool isClickedButton = false;
+        private MonoScript OldMonoScript = null;
+        private bool isConfiguration = false;
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
@@ -25,61 +26,31 @@ namespace GameArchitecture
 
             MonoScript = EditorGUI.ObjectField(new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight), "Manager", MonoScript, typeof(MonoScript), false) as MonoScript;
 
-            if (MonoScript!=null&& !MonoScript.GetClass().GetInterfaces().Contains(typeof(IManager)) && !MonoScript.GetClass().GetInterfaces().Any(t => t.Name.Substring(0, t.Name.IndexOf('`')) == typeof(IManager).Name))
-                MonoScript = null;
+            if (MonoScript != null && !MonoScript.GetClass().GetInterfaces().Contains(typeof(IManager)) && !MonoScript.GetClass().GetInterfaces().Any(t => t.Name.Substring(0, t.Name.IndexOf('`')) == typeof(IManager).Name))
+                MonoScript = OldMonoScript;
 
-            if (MonoScript != NewMonoScript)
+            if (MonoScript != OldMonoScript)
             {
-                property.FindPropertyRelative("Configuration").managedReferenceValue = null;
-                isClickedButton = false;
-                NewMonoScript = MonoScript;
-            }
+                isConfiguration = false;
+                OldMonoScript = MonoScript;
 
-            if (MonoScript != null)
-            {
-                if (!isClickedButton && GUI.Button(new Rect(position.x, position.y + EditorGUIUtility.singleLineHeight, position.width, EditorGUIUtility.singleLineHeight), "Add existing configuration"))
+                if (MonoScript != null && MonoScript.GetClass().GetInterfaces().Any(t => t.Name.Substring(0, t.Name.IndexOf('`')) == typeof(IManager).Name))
                 {
-                    Type type = MonoScript.GetClass();
-                    string[] assets = AssetDatabase.GetAllAssetPaths().Where(name => name.EndsWith(".cs")).ToArray();
+                    property.FindPropertyRelative("Configuration").managedReferenceValue = Activator.CreateInstance(MonoScript.GetClass().GetInterfaces().Where(t => t.Name.Substring(0, t.Name.IndexOf('`')) == typeof(IManager).Name).First().GenericTypeArguments[0]);
 
-                    foreach (string asset in assets)
-                    {
-                        MonoScript element = AssetDatabase.LoadAssetAtPath<MonoScript>(asset);
-
-                        if (element != null && element.GetClass() != null)
-                        {
-                            Attribute attribute = Attribute.GetCustomAttribute(element.GetClass(), typeof(ConfigurationAttribute));
-
-                            if (attribute != null && attribute as ConfigurationAttribute !=null && (attribute as ConfigurationAttribute).ManagerType == type)
-                            {
-                                property.FindPropertyRelative("Configuration").managedReferenceValue = Activator.CreateInstance(element.GetClass());
-                                isClickedButton = true;
-                                break;
-                            }
-                        }
-                    }
-
-                    //if (!isClickedButton)
-                        //Debug.LogWarning("configuration class for this IManager is not ");
-                    //    EditorGUI.HelpBox(new Rect(position.x, position.y + EditorGUIUtility.singleLineHeight * 2, position.width, EditorGUIUtility.singleLineHeight),"this class is not derived of IManager",MessageType.Warning);
-                        //EditorGUI.HelpBox(new Rect(position.x, position.y + EditorGUIUtility.singleLineHeight * 2, position.width, EditorGUIUtility.singleLineHeight),"this class does not have attribute Configuration",MessageType.Warning);
+                    isConfiguration = true;
                 }
-
-                if (isClickedButton)
-                    EditorGUI.PropertyField(new Rect(position.x, position.y + EditorGUIUtility.singleLineHeight * 2, position.width, EditorGUIUtility.singleLineHeight), property.FindPropertyRelative("Configuration"), true);
             }
-            else
-                isClickedButton = false;
+
+            if (isConfiguration)
+                EditorGUI.PropertyField(new Rect(position.x, position.y + EditorGUIUtility.singleLineHeight, position.width, EditorGUIUtility.singleLineHeight), property.FindPropertyRelative("Configuration"), true);
 
             EditorGUI.EndProperty();
         }
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            if (isClickedButton)
-                return EditorGUIUtility.singleLineHeight*2 + EditorGUI.GetPropertyHeight(property.FindPropertyRelative("Configuration")) + EditorGUIUtility.standardVerticalSpacing;
-
-            if (MonoScript != null)
-                return EditorGUIUtility.singleLineHeight *2 + EditorGUIUtility.standardVerticalSpacing;
+            if (isConfiguration)
+                return EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing + EditorGUI.GetPropertyHeight(property.FindPropertyRelative("Configuration"));
 
             return EditorGUIUtility.singleLineHeight;
         }
