623b0ced909c953fb585c3bd87b28b5a743a7077
Dema <67798322+DemaReaktor@users.noreply.github.com>
2023-02-12 13:00:26 +0200

    Save

--- Assets/Scripts/Architecture/ManagerConfiguration.cs
+++ Assets/Scripts/Architecture/ManagerConfiguration.cs
@@ -2,80 +2,79 @@ using UnityEngine;
 using UnityEditor;
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace GameArchitecture
 {
+    /// <summary>
+    /// class that save parametres of manager in inspector
+    /// </summary>
     [Serializable]
     public class ManagerConfiguration
     {
-        [Tooltip("class should be direved by IManager")]
+        /// <summary>
+        /// file that has manager
+        /// </summary>
         public MonoScript MonoScript;
+        /// <summary>
+        /// class that has patametres of manager
+        /// Can be null
+        /// </summary>
         [SerializeReference] public object Configuration;
     }
 
     [CustomPropertyDrawer(typeof(ManagerConfiguration))]
     public class ManagerConfigurationEditor : PropertyDrawer
     {
-        private MonoScript MonoScript = null;
-        private MonoScript OldMonoScript = null;
-        private bool isConfiguration = false;
+        //dictionaries that save parametres of every SerializedProperty
+        private Dictionary<string, MonoScript> OldMonoScript = new Dictionary<string, MonoScript>();
+        private Dictionary<string, bool> isConfiguration = new Dictionary<string, bool>();
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.BeginProperty(position, label, property);
 
-            MonoScript = EditorGUI.ObjectField(new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight), "Manager", MonoScript, typeof(MonoScript), false) as MonoScript;
+            //get value of class
+            MonoScript MonoScript = EditorGUI.ObjectField(new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight),
+                new GUIContent("Manager", "class should be direved by IManager"), property.FindPropertyRelative("MonoScript").objectReferenceValue, typeof(MonoScript), false) as MonoScript;
 
+            //check if this class is derived by IManager otherwise property dont change value
             if (MonoScript != null && !MonoScript.GetClass().GetInterfaces().Contains(typeof(IManager)) && !MonoScript.GetClass().GetInterfaces().Any(t => t.Name.Substring(0, t.Name.IndexOf('`')) == typeof(IManager).Name))
-                MonoScript = OldMonoScript;
+                    MonoScript = OldMonoScript.Keys.Contains(property.propertyPath)? OldMonoScript[property.propertyPath]: null;
 
-            if (MonoScript != OldMonoScript)
+            //set value
+            property.FindPropertyRelative("MonoScript").objectReferenceValue = MonoScript;
+
+            //if propery value of manager is changed
+            if ((!OldMonoScript.Keys.Contains(property.propertyPath) && MonoScript != null) || (OldMonoScript.Keys.Contains(property.propertyPath) && MonoScript != OldMonoScript[property.propertyPath]))
             {
-                isConfiguration = false;
-                OldMonoScript = MonoScript;
+                isConfiguration[property.propertyPath] = false;
 
+                //if manager has configuration ( if manager has generic argument)
                 if (MonoScript != null && MonoScript.GetClass().GetInterfaces().Any(t => t.Name.Substring(0, t.Name.IndexOf('`')) == typeof(IManager).Name))
                 {
+                    //crete this configuration and set into value
                     property.FindPropertyRelative("Configuration").managedReferenceValue = Activator.CreateInstance(MonoScript.GetClass().GetInterfaces().Where(t => t.Name.Substring(0, t.Name.IndexOf('`')) == typeof(IManager).Name).First().GenericTypeArguments[0]);
 
-                    isConfiguration = true;
+                    isConfiguration[property.propertyPath] = true;
                 }
             }
 
-            if (isConfiguration)
+            OldMonoScript[property.propertyPath] = MonoScript;
+
+            //show property
+            if (isConfiguration.Keys.Contains(property.propertyPath) && isConfiguration[property.propertyPath])
                 EditorGUI.PropertyField(new Rect(position.x, position.y + EditorGUIUtility.singleLineHeight, position.width, EditorGUIUtility.singleLineHeight), property.FindPropertyRelative("Configuration"), true);
 
             EditorGUI.EndProperty();
         }
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            if (isConfiguration)
+            //if configuration is
+            if (isConfiguration.Keys.Contains(property.propertyPath) && isConfiguration[property.propertyPath])
                 return EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing + EditorGUI.GetPropertyHeight(property.FindPropertyRelative("Configuration"));
 
             return EditorGUIUtility.singleLineHeight;
-        }
-    }
-
-    [Serializable]
-    public class Pair
-    {
-        public string Key;
-        public string Value;
-    }
-
-    [CustomPropertyDrawer(typeof(Pair))]
-    public class PairEditor : PropertyDrawer
-    {
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-        {
-            EditorGUI.BeginProperty(position, label, property);
-
-            var key = property.FindPropertyRelative("Key");
-            key.stringValue = EditorGUI.TextField(new Rect(position.x, position.y, position.width / 2, position.height), "", key.stringValue);
-
-            var value = property.FindPropertyRelative("Value");
-            value.stringValue = EditorGUI.TextField(new Rect(position.x + position.width / 2, position.y, position.width / 2, position.height), "", value.stringValue);
-            EditorGUI.EndProperty();
         }
     }
 }
