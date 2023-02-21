1f9d9a7fac07c9440d8993c0476621a12ef97fa2
Dema <67798322+DemaReaktor@users.noreply.github.com>
2023-02-21 21:43:35 +0200

    ScrollCursorImage

--- Assets/Scripts/menu/ScrollCusorImage.cs
+++ Assets/Scripts/menu/ScrollCusorImage.cs
@@ -1,18 +1,49 @@
-using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEditor;
+using UnityEditor.UI;
+using KeyCodeController;
 
-public class ScrollCusorImage : MonoBehaviour
+public class ScrollCusorImage : Image
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Sprite DefaultSprite;
+    public Sprite KeyCodeSetterSprite;
 
-    // Update is called once per frame
+    private List<RaycastResult> list;
+    private PointerEventData pointerData;
+
+    protected override void Start()
+    {
+        list = new List<RaycastResult>();
+        pointerData = new PointerEventData(EventSystem.current);
+
+        base.Start();
+    }
     void Update()
     {
-        
+        if (Application.isPlaying)
+        {
+            pointerData.position = Input.mousePosition;
+            EventSystem.current.RaycastAll(pointerData, list);
+
+            sprite = list.Count != 0 && list.First().gameObject.transform.TryGetComponent(out KeyCodeSetter keyCodeSetter) ? KeyCodeSetterSprite : DefaultSprite;
+        }
+    }
+}
+[CustomEditor(typeof(ScrollCusorImage))]
+public class ScrollCusorImageEditor : ImageEditor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        EditorGUILayout.Space();
+        (serializedObject.targetObject as ScrollCusorImage).DefaultSprite = EditorGUILayout.ObjectField(new GUIContent("DefaultSprite", ""),
+            serializedObject.FindProperty("DefaultSprite").objectReferenceValue,typeof(Sprite),true) as Sprite;
+        (serializedObject.targetObject as ScrollCusorImage).KeyCodeSetterSprite = EditorGUILayout.ObjectField(new GUIContent("KeyCodeSetterSprite", ""),
+            serializedObject.FindProperty("KeyCodeSetterSprite").objectReferenceValue, typeof(Sprite), true) as Sprite;
     }
 }
